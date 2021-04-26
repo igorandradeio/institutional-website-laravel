80989204317bd792a61e3be576b7c5934ae47a19
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-04-26 11:06:30 -0300

    New contact form request

--- app/Http/Controllers/Website/ContactController.php
+++ app/Http/Controllers/Website/ContactController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Website;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Contact;
+use App\Notifications\NewEmail;
+use Illuminate\Support\Facades\Notification;
+use App\Http\Requests\ContactFormRequest;
 
 class ContactController extends Controller
 {
@@ -17,9 +21,23 @@ class ContactController extends Controller
         return view("website.contact.index");
     }
 
-    public function form(){
-
-    
+    public function form(ContactFormRequest $request)
+    {
+        // save contact-us form in database
+        $contact = Contact::create($request->all());
+        
+        Notification::route('mail', config('mail.from.address'))
+        ->notify(new NewEmail($contact));
+        
+        /*
+        return redirect()->route('website.contact')->with([
+            'success' => true,
+            'message' => 'Your message has been sent.'
+        ]);
+        */
+        
+        toastr()->success('Your message has been set');
+        return back();
     }
 
 
