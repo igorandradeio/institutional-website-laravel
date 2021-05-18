5e6e29e10e429a1a8fddf0c489c3d0ace2e0cfa0
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-05-18 13:11:37 -0300

    Display all products by category

--- resources/views/website/category/show.blade.php
+++ resources/views/website/category/show.blade.php
@@ -1,43 +1,40 @@
 @extends('layouts.website')
-
 @section('title', 'Products')
-
 @section('content')
 <!-- ==== Breadcrumbs ==== -->
 <div class="breadcrumbs">
     <div class="container">
         <h2>Products</h2>
     </div>
-</div><!-- End Breadcrumbs -->
-
+</div>
+<!-- End Breadcrumbs -->
 <!-- ==== Products Section ==== -->
 <section id="Product">
-    
     <div class="container">
-
-        <div class="row">
-            
-            <div class="col-lg-6 order-2 order-lg-1">
-                <img src="{{asset('images/about/work-about-us.jpeg')}}" class="img-fluid" alt="">
-            </div>
-            
-            <div class="col-lg-6 pt-4 pt-lg-0 order-1 order-lg-2 content">
-                <h3>{{$slug}}</h3>
-                <ul>
-                    <li><i class="bi bi-cash"></i> Price: 22,00</li>
-                    <li><i class="bi bi-tag"></i> Category: {{$slug}}</li>
-
-                </ul>
-                <h5>Description</h5>
-                <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet consequatur dolorem accusantium blanditiis culpa, odio itaque nesciunt mollitia et eos ratione doloribus, illum voluptates facere numquam? Quaerat deserunt molestiae sunt.
-                </p>
-            </div>
-
+        <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+            <h1 class="display-4 fw-normal">Meet our Products</h1>
+            <p class="fs-5 text-muted">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit dicta deleniti exercitationem eveniet esse fuga a iure. Quo odit animi nulla consequuntur ipsum mollitia, iusto, sed error rem natus esse!</p>
         </div>
-
-
+        <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            @foreach($category->products as $product)
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm border-primary">
+                    <a href="{{route('website.product.show', ['product' => $product])}}" >
+                        <div class="card-header py-3 text-white bg-primary border-primary">
+                            <h4 class="my-0 fw-normal">{{$product->name}}</h4>
+                        </div>
+                        <img class="card-img-top" src="{{asset($product->image)}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>{{$product->description}}</li>
+                            </ul>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            @endforeach
+        </div>
     </div>
-
-</section><!-- End Products Section -->
+</section>
+<!-- End Products Section -->
 @endsection
