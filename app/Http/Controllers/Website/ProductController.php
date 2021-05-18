f585a0e316dae765d58148a31f9a1680c662be8d
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-05-18 12:26:30 -0300

    Add product controller

---
+++ app/Http/Controllers/Website/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Website;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('website.category.product');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = DB::table('products')->where('id', $id)->first();
+        $category = DB::table('categories')->where('id', $product->category_id)->first();
+
+        return view('website.category.product', [
+            'product' => $product, 'category' => $category->name
+            ]);
+
+    }
+
+}
