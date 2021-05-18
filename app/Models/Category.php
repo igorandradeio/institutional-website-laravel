ba949bfe4006c247b62499f48f0453d7d759263c
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-05-18 12:22:36 -0300

    add image field

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Category extends Model
 {
@@ -16,6 +18,7 @@ class Category extends Model
      */
     protected $fillable = [
         'name',
+        'image'
     ];
 
     protected $dates = [
