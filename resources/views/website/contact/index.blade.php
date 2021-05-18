bdb0db98539fca2dcef9ceb621c5e6e3da4179ab
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-05-18 13:04:44 -0300

    Add validation

--- resources/views/website/contact/index.blade.php
+++ resources/views/website/contact/index.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.website')
-
 @section('title', 'Contact us')
-
 @section('content')
 <!-- ==== Breadcrumbs ==== -->
 <div class="breadcrumbs">
@@ -9,74 +7,80 @@
         <h2>Contact us</h2>
         <p>Do you have any questions? Please do not hesitate to contact us directly.</p>
     </div>
-</div><!-- End Breadcrumbs -->
-
+</div>
+<!-- End Breadcrumbs -->
 <!-- ==== Contact us Section ==== -->
 <section id="contact" class="contact">
     <!-- Google Maps -->
     <div>
         <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7678.164931829471!2d-47.862328206309854!3d-15.79960510991473!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc7c45a6f5981ca10!2sPal%C3%A1cio%20do%20Planalto!5e0!3m2!1spt-BR!2sus!4v1618083554676!5m2!1spt-BR!2sus" frameborder="0" allowfullscreen></iframe>
     </div>
-
+    <!-- Container -->
     <div class="container">
-
         <div class="row mt-5">
-
             <div class="col-lg-4">
                 <div class="info">
-
                     <div class="address">
                         <i class="bi bi-geo-alt"></i>
                         <h4>Location:</h4>
                         <p>Street, City, State, Country</p>
                     </div>
-
                     <div class="email">
                         <i class="bi bi-envelope"></i>
                         <h4>Email:</h4>
                         <p>email@example.com</p>
                     </div>
-
                     <div class="phone">
                         <i class="bi bi-phone"></i>
                         <h4>Call:</h4>
                         <p>+0 0000 12345 67</p>
                     </div>
-
                 </div>
             </div>
-
             <div class="col-lg-8 mt-5 mt-lg-0">
                 <form class="contact-form" action="{{route('website.contact.form')}}" method="post">
                     @csrf
+                    @if(session('success'))
+                    <div>
+                        {{session('message')}}
+                    </div>
+                    @endif
                     <div class="row">
                         <div class="col-md-6 form-group">
-                            <input type="text" class="form-control" name="name" id="name" placeholder="Your Name" required>
+                            @error('name')
+                            <div>{{ $message }}</div>
+                            @enderror
+                            <input type="text" class="form-control" name="name" id="name" placeholder="Your Name" value="{{old('name')}}" autocomplete="off">
                         </div>
                         <div class="col-md-6 form-group mt-3 mt-md-0">
-                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                            @error('email')
+                            <div>{{ $message }}</div>
+                            @enderror
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" value="{{old('email')}}">
                         </div>
                     </div>
-
                     <div class="row">
                         <div class="form-group mt-3">
-                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                            @error('subject')
+                            <div>{{ $message }}</div>
+                            @enderror
+                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" value="{{old('subject')}}" autocomplete="off" >
                         </div>
                     </div>
-
                     <div class="row">
                         <div class="form-group mt-3">
-                            <textarea class="form-control" name="message" id="message" rows="5" placeholder="Message" required></textarea>
+                            @error('message')
+                            <div>{{ $message }}</div>
+                            @enderror
+                            <textarea class="form-control" name="message" id="message" rows="5" placeholder="Message">{{old('message')}}</textarea>
                         </div>
                     </div>
-
                     <div class="text-center"><button type="submit">Send Message</button></div>
                 </form>
             </div>
-
         </div>
-
+        <!-- /.row -->
     </div>
-
-</section><!-- End Contact us Section -->
+</section>
+<!-- End Contact us Section -->
 @endsection
