49ea3230d2ebef067ecb23748cd8fc6f66926531
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-05-18 13:09:59 -0300

    Show all categories

--- resources/views/website/category/index.blade.php
+++ resources/views/website/category/index.blade.php
@@ -1,83 +1,40 @@
 @extends('layouts.website')
-
 @section('title', 'Products')
-
 @section('content')
 <!-- ==== Breadcrumbs ==== -->
 <div class="breadcrumbs">
     <div class="container">
         <h2>Products</h2>
     </div>
-</div><!-- End Breadcrumbs -->
-
+</div>
+<!-- End Breadcrumbs -->
 <!-- ==== Products Section ==== -->
 <section id="Product">
- 
     <div class="container">
         <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-            <h1 class="display-4 fw-normal">Meet our Products</h1>
+            <h1 class="display-4 fw-normal">Meet our Categories</h1>
             <p class="fs-5 text-muted">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit dicta deleniti exercitationem eveniet esse fuga a iure. Quo odit animi nulla consequuntur ipsum mollitia, iusto, sed error rem natus esse!</p>
         </div>
-    
         <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-
+            @foreach($categories as $category)
             <div class="col">
                 <div class="card mb-4 rounded-3 shadow-sm border-primary">
-                    <div class="card-header py-3 text-white bg-primary border-primary">
-                        <h4 class="my-0 fw-normal">Enterprise</h4>
-                    </div>
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>30 users included</li>
-                            <li>15 GB of storage</li>
-                            <li>Phone and email support</li>
-                            <li>Help center access</li>
-                        </ul>
-                        <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
-                    </div>
+                    <a href="{{route('website.category.show', ['category' => $category])}}" >
+                        <img class="card-img-top" src="{{asset( $category->image )}}" class="card-img-top" alt="...">
+                        <div class="card-header py-3 text-white bg-primary border-primary">
+                            <h4 class="my-0 fw-normal">{{ $category->name }}</h4>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>{{ $category->description }}</li>
+                            </ul>
+                        </div>
+                    </a>
                 </div>
             </div>
-
-            <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sm border-primary">
-                    <div class="card-header py-3 text-white bg-primary border-primary">
-                        <h4 class="my-0 fw-normal">Enterprise</h4>
-                    </div>
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>30 users included</li>
-                            <li>15 GB of storage</li>
-                            <li>Phone and email support</li>
-                            <li>Help center access</li>
-                        </ul>
-                        <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sm border-primary">
-                    <div class="card-header py-3 text-white bg-primary border-primary">
-                        <h4 class="my-0 fw-normal">Enterprise</h4>
-                    </div>
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>30 users included</li>
-                            <li>15 GB of storage</li>
-                            <li>Phone and email support</li>
-                            <li>Help center access</li>
-                        </ul>
-                        <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
-                    </div>
-                </div>
-            </div>
-
+            @endforeach
         </div>
-
     </div>
-
-</section><!-- End Products Section -->
+</section>
+<!-- End Products Section -->
 @endsection
