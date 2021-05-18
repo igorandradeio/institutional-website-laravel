56aba7e6f48b46228c405c430538e9f30ba37939
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-05-18 13:13:47 -0300

    Updating the about us page

--- resources/views/website/about/index.blade.php
+++ resources/views/website/about/index.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.website')
-
 @section('title', 'About us')
-
 @section('content')
 <!-- ==== Breadcrumbs ==== -->
 <div class="breadcrumbs">
@@ -9,19 +7,15 @@
         <h2>About us</h2>
         <p>Our company culture, and how we do things</p>
     </div>
-</div><!-- End Breadcrumbs -->
-
+</div>
+<!-- End Breadcrumbs -->
 <!-- ==== About us Section ==== -->
 <section id="about" class="about">
- 
     <div class="container">
-
         <div class="row">
-            
             <div class="col-lg-6 order-1 order-lg-2">
                 <img src="{{asset('images/about/work-about-us.jpeg')}}" class="img-fluid" alt="people doing office works">
             </div>
-            
             <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
                 <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
                 <ul>
@@ -30,14 +24,11 @@
                     <li><i class="bi bi-check-circle"></i> Eveniet consequatur dolorem accusantium blanditiis culpa, odio itaque nesciunt mollitia et eos ratione doloribus.</li>
                 </ul>
                 <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet consequatur dolorem accusantium blanditiis culpa, odio itaque nesciunt mollitia et eos ratione doloribus, illum voluptates facere numquam? Quaerat deserunt molestiae sunt.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet consequatur dolorem accusantium blanditiis culpa, odio itaque nesciunt mollitia et eos ratione doloribus, illum voluptates facere numquam? Quaerat deserunt molestiae sunt.
                 </p>
             </div>
-
         </div>
-
-
     </div>
-
-</section><!-- End Contact us Section -->
+</section>
+<!-- End Contact us Section -->
 @endsection
