ac0b0794e5abab0b0d4bbef8150a0a4952342c5b
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-05-26 10:40:07 -0300

    Create slug on creation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 
 class Category extends Model
@@ -18,14 +19,24 @@ class Category extends Model
      */
     protected $fillable = [
         'name',
-        'image'
+        'description',
+        'image',
+        'slug'
     ];
 
     protected $dates = [
         'deleted_at',
     ];
 
+    protected static function booted()
+    {
+        static::creating(function ($category) {
+            $category->slug = Str::of($category->name)->slug('-');
+        });
+    }
+
     public function products(){
         return $this->hasMany(Product::class);
     }
+
 }
