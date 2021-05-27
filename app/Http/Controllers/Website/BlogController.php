45fc28dff9baef1c3f657aabd887e6b389807d4d
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-05-27 11:33:12 -0300

    Remove comment

--- app/Http/Controllers/Website/BlogController.php
+++ app/Http/Controllers/Website/BlogController.php
@@ -16,8 +16,7 @@ class BlogController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //$posts = Post::all();
         $posts = Post::simplePaginate(5);
         return view("website.blog.index", compact('posts'));
     }
-}
+} 
