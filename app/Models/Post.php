834c03c952f4af1ccb1c7dc19f0a701fce45c0c0
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-05-27 10:02:12 -0300

    Create slug on creation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -20,12 +20,20 @@ class Post extends Model
         'title',
         'imagem',
         'content',
+        'slug'
     ];
 
     protected $dates = [
         'deleted_at',
     ];
     
+    protected static function booted()
+    {
+        static::creating(function ($post) {
+            $post->slug = Str::of($post->title)->slug('-');
+        });
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -20,12 +20,20 @@ class Product extends Model
         'name',
         'description',
         'image',
+        'slug'
     ];
 
     protected $dates = [
         'deleted_at',
     ];
 
+    protected static function booted()
+    {
+        static::creating(function ($product) {
+            $product->slug = Str::of($product->name)->slug('-');
+        });
+    }
+    
     public function category(){
         return $this->belongsTo(Category::class);
     }
