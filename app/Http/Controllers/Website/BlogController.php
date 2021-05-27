8bd140b9c172029800373c5693b52519a4f6c252
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-05-27 11:16:28 -0300

    Add Laravel simple pagination

--- app/Http/Controllers/Website/BlogController.php
+++ app/Http/Controllers/Website/BlogController.php
@@ -16,8 +16,8 @@ class BlogController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $posts = Post::all();
-        
+        //$posts = Post::all();
+        $posts = Post::simplePaginate(5);
         return view("website.blog.index", compact('posts'));
     }
 }
