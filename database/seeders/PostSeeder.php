0d2f0d644677c71e8c31f2177e997d39c0360f6a
Igor <oigorwebs@gmail.com>
2021-11-29 00:28:13 -0300

    Create post seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
@@ -13,6 +14,11 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Post::create([
+            'user_id' => '1',
+            'title' => "I'M A POST",
+            'image' => 'images/blog/example-post.jpeg',
+            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod rem deleniti fugiat consequatur repudiandae atque officiis et assumenda nihil, enim veritatis dolorem mollitia repellat asperiores ducimus nesciunt excepturi laborum. Mollitia.'
+        ]);
     }
 }
