38fd47aaf6d672d8dbf0c8848f0cefa33441d576
Igor <oigorwebs@gmail.com>
2021-11-29 00:06:50 -0300

    Add product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -13,6 +14,18 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Product::create([
+            'category_id' => '1',
+            'name' => 'Headset Gamer HP',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ut ullam error quo! Ea, quod? Inventore molestiae enim nobis cupiditate deserunt ut laborum libero non dolorum. Doloremque debitis mollitia ipsam.',
+            'image' => 'images/products/headset-gamer-hp.jpg'
+        ]);
+
+        Product::create([
+            'category_id' => '2',
+            'name' => 'Mouse Gamer Logitech g403',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ut ullam error quo! Ea, quod? Inventore molestiae enim nobis cupiditate deserunt ut laborum libero non dolorum. Doloremque debitis mollitia ipsam.',
+            'image' => 'images/products/mouse-gamer-g403.jpg'
+        ]);
     }
 }
