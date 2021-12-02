32a7d36462dc7aad2ac3ea4985d0d78a39a78537
Igor <oigorwebs@gmail.com>
2021-12-02 04:05:02 -0300

    Add admin posts view

---
+++ resources/views/admin/blog/index.blade.php
@@ -0,0 +1,63 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+<h1>All Posts</h1>
+@stop
+
+@section('content')
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">All Posts</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th style="width: 10px">#</th>
+                                    <th>Title</th>
+                                    <th>Date</th>
+                                    <th>Author</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse($posts as $post)
+                                <tr>
+                                    <td>{{$post->id}}</td>
+                                    <td>{{$post->title}}</td>
+                                    <td>{{$post->created_at->format('d/m/Y H:i:s')}}</td>
+                                    <td>{{$post->user->name}}</td>
+                                </tr>
+                                @empty
+                                <div>No post found</div>
+                                @endforelse
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer clearfix">
+                        <ul class="pagination pagination-sm m-0 float-right">
+                            {{ $posts->links() }}
+                        </ul>
+                    </div>
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+</section>
+@stop
+
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+@stop
