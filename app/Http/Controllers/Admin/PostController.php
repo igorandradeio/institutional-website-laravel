e99dbe05d7ab549fd515c0119b7b965a575df2de
Igor <oigorwebs@gmail.com>
2021-12-04 04:06:07 -0300

    Add create, edit and update functions

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,21 +3,72 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PostFormRequest;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
     public function index()
     {
         $posts = Post::simplePaginate(30);
-
         return view('admin.blog.index', compact('posts'));
     }
 
     public function newPost()
     {
 
-        return view('admin.blog.post');
+        return view('admin.blog.create');
+    }
+
+    public function store(PostFormRequest $request)
+    {
+        if ($request->image->isValid()) {
+            $fileName = Str::of($request->title)->slug('-') . '.' . $request->image->getClientOriginalExtension();;
+            $imagePath = $request->image->storeAs('posts-images', $fileName);
+        }
+
+        Post::create([
+            'user_id' => auth()->user()->id,
+            'title' => $request->title,
+            'image' => $imagePath,
+            'content' => $request->content,
+        ]);
+
+        return back();
+    }
+
+    public function edit($id)
+    {
+
+        $post = Post::find($id);
+
+        if (!$post) {
+            return redirect()->route('admin');
+        }
+
+        return view('admin.blog.edit', compact("post"));
+    }
+
+    public function update(PostFormRequest $request, $id)
+    {
+
+        $post = Post::find($id);
+
+        if ($request->image->isValid()) {
+            $fileName = Str::of($request->title)->slug('-') . '.' . $request->image->getClientOriginalExtension();;
+            $imagePath = $request->image->storeAs('posts-images', $fileName);
+        }
+
+        $post->update([
+            'user_id' => auth()->user()->id,
+            'title' => $request->title,
+            'image' => $imagePath,
+            'content' => $request->content,
+        ]);
+
+        return back();
     }
 }
