9529211bafa78b99233cb8fbea7dc4d96ae5abbc
Igor <oigorwebs@gmail.com>
2021-12-04 04:04:34 -0300

    Create Post form

---
+++ resources/views/admin/blog/partials/form.blade.php
@@ -0,0 +1,37 @@
+@csrf
+<div class="card-body">
+    <div class="form-group">
+        <label>Title</label>
+        @error('title')
+        <div>{{ $message }}</div>
+        @enderror
+        <input type="text" value="{{$post->title ?? old('title') }}" class="form-control" name="title" placeholder="Enter Title here">
+    </div>
+</div>
+<!-- /.card-body -->
+<div class="card-body">
+    <div class="form-group">
+        <label>Featured image</label>
+        @error('image')
+        <div>{{ $message }}</div>
+        @enderror
+        <div class="custom-file">
+            <input type="file" name="image" id="image" class="custom-file-input">
+            <label class="custom-file-label">Choose file</label>
+        </div>
+    </div>
+</div>
+<!-- /.card-body -->
+<div class="card-body">
+    <div class="form-group">
+        <label>Text</label>
+        @error('content')
+        <div>{{ $message }}</div>
+        @enderror
+        <textarea id="summernote" name="content">{{ $post->content ?? old('content') }}</textarea>
+    </div>
+</div>
+<!-- /.card-body -->
+<div class="card-footer">
+    <button type="submit" class="btn btn-primary">Submit</button>
+</div>
