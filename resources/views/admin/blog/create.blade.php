44e6d719ab2af889cfb8070f5c5ba6e59476e1ad
Igor <oigorwebs@gmail.com>
2021-12-04 04:05:01 -0300

    Add post create page

---
+++ resources/views/admin/blog/create.blade.php
@@ -0,0 +1,46 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+<h1>Add new post</h1>
+@stop
+
+@section('content')
+<!-- Main content -->
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Form</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form action="{{route('admin.post.store')}}" method="post" enctype="multipart/form-data">
+                    @include('admin.blog.partials.form')
+                </form>
+            </div>
+            <!-- /.card -->
+        </div>
+    </div>
+</section>
+<!-- /.content -->
+</div>
+@stop
+
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+<script>
+    $(function() {
+        // Summernote
+        $('#summernote').summernote()
+    })
+    $(function() {
+        bsCustomFileInput.init();
+    });
+</script>
+@stop
