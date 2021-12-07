3eec3c1c5ec2fb10b884ce348494b198f986e9e3
Igor <oigorwebs@gmail.com>
2021-12-07 16:17:46 -0300

    Add successful message

--- resources/views/admin/blog/index.blade.php
+++ resources/views/admin/blog/index.blade.php
@@ -10,6 +10,13 @@
 <!-- Main content -->
 <section class="content">
     <div class="container-fluid">
+        @if(session('message'))
+        <div class="alert alert-success alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <h5><i class="icon fas fa-check"></i> Alert!</h5>
+            {{session('message')}}
+        </div>
+        @endif
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
