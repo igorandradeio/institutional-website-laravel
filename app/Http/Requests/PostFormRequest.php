7837513c135c43539c2010964407b475f05d9ad1
Igor <oigorwebs@gmail.com>
2021-12-08 18:17:26 -0300

    Add new rules

--- app/Http/Requests/PostFormRequest.php
+++ app/Http/Requests/PostFormRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class PostFormRequest extends FormRequest
 {
@@ -23,19 +24,37 @@ class PostFormRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'title' => 'required|min:2|max:100',
+        $id = $this->id;
+
+        $rules = [
+            'title' => [
+                'required',
+                'min:2',
+                'max:100',
+                Rule::unique('posts')->ignore($id),
+            ],
             'image' => ['required', 'image'],
             'content' => 'required|min:2|max:50000'
         ];
+
+        //image isn't required if the user doesn't upload a new image
+        if ($this->method() == 'PUT') {
+            $rules['image'] = ['nullable', 'image'];
+        }
+
+        return $rules;
     }
 
+    //custom error messages
+    /*
     public function messages()
     {
+
         return [
             'name.required' => 'The title field is required.',
             'name.image' => 'The image field is required.',
             'name.content' => 'The content field is required.',
         ];
     }
+    */
 }
