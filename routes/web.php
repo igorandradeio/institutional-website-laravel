2a01d5950099f0db9f07cf4d11bfb1b0707041e5
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-12-21 01:48:40 -0300

    Add post search route

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::namespace("App\Http\Controllers\Website")->group(function () {
 Route::namespace("App\Http\Controllers\Admin")->middleware(['auth'])->group(function () {
     Route::get("/admin", "HomeController@index")->name('admin');
     Route::get("/admin/post", "PostController@index")->name('admin.post');
+    Route::any("/admin/post/search", "PostController@search")->name('admin.post.search');
     Route::post("/admin/post/create'", "PostController@store")->name('admin.post.store');
     Route::get("/admin/post/edit/{id}", "PostController@edit")->name('admin.post.edit');
     Route::put("/admin/post/update/{id}", "PostController@update")->name('admin.post.update');
