a3b401853c49aa3d6629a4150c02e24919a3d8a2
Igor Cerqueira Andrade <oigorwebs@gmail.com>
2021-12-21 01:53:34 -0300

    Add search form

--- resources/views/admin/blog/index.blade.php
+++ resources/views/admin/blog/index.blade.php
@@ -21,6 +21,30 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
+                        <h3 class="card-title">Search</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <form action="{{route('admin.post.search')}}" method="post">
+                            @csrf
+                            <div class="input-group mb-3">
+                                <input type="text" name="filter" value="{{ $filters['filter'] ?? '' }}" class="form-control">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary" type="submit">Search</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
                         <h3 class="card-title">All Posts</h3>
                     </div>
                     <!-- /.card-header -->
@@ -52,7 +76,11 @@
                     <!-- /.card-body -->
                     <div class="card-footer clearfix">
                         <ul class="pagination pagination-sm m-0 float-right">
+                            @if (isset($filters))
+                            {{ $posts->appends($filters)->links() }}
+                            @else
                             {{ $posts->links() }}
+                            @endif
                         </ul>
                     </div>
                 </div>
